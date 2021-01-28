c08fc1bacadeaa5aff3e508163a060607e3521a4
Michael Ryan <michaelryan2895@gmail.com>
2021-01-27 20:06:16 -0500

    all files except app 1

---
+++ app2.js
@@ -0,0 +1,15 @@
+
+let title = prompt('Nic or Cage?');
+console.log(title);
+
+if (title == 'Nic'){
+    document.write('Welcome Nic, ');
+} else if (title == 'Cage'){
+    document.write('Welcome Cage, ');
+} else if (title == 'nic'){
+    document.write('Welcome Nic, ');
+} else if (title == 'cage'){
+    document.write('Welcome Cage, ');
+} else {
+    document.write('Welcome Nicolas Kim Coppola, ');
+}
---
+++ app3.js
@@ -0,0 +1,8 @@
+let userName = prompt('What is your name?')
+console.log(userName);
+
+if (userName == 'uncle'){
+    document.write("Francis Ford Coppola");
+} else {
+    document.write(userName);
+}
