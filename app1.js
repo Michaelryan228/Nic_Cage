3a01528b29f18f2ff48b6b518fde5a12cc348e9d
Michael Ryan <michaelryan2895@gmail.com>
2021-01-28 15:03:07 -0500

    function added

--- app1.js
+++ app1.js
@@ -1,15 +1,19 @@
-let today = new Date();
-let hourNow = today.getHours();
-let greeting;
+function hourGreet() {
+    let today = new Date();
+    let hourNow = today.getHours();
+    let greeting;
 
-if (hourNow > 18) {
-    greeting = 'It is a good evening for Nic!';
-} else if (hourNow > 0) {
-    greeting = 'It is a good afternoon... for Nic!';
-} else if (hourNow > 0) {
-    greeting = 'Its a pretty good morning.  But only for Nic!';
-} else {
-    greeting = 'Welcome to my website, I am Nic!';
+    if (hourNow > 18) {
+        greeting = 'It is a good evening for Nic!';
+    } else if (hourNow > 0) {
+        greeting = 'It is a good afternoon... for Nic!';
+    } else if (hourNow > 0) {
+        greeting = 'Its a pretty good morning.  But only for Nic!';
+    } else {
+        greeting = 'Welcome to my website, I am Nic!';
+    }
+
+    document.write('<h3>' + greeting + '</h3>');
 }
 
-document.write('<h3>' + greeting + '</h3>');
+hourGreet();
