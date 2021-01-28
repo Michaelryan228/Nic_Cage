cfc5a99ad5f5b3b9e46aacb2812bd2b590d7b153
Michael Ryan <michaelryan2895@gmail.com>
2021-01-28 15:31:37 -0500

    functions for app 2 and 3

--- app2.js
+++ app2.js
@@ -1,15 +1,19 @@
+function enterName() {
+    let title = prompt('Nic or Cage?');
+    console.log(title);
 
-let title = prompt('Nic or Cage?');
-console.log(title);
+    if (title == 'Nic') {
+        document.write('Welcome Nic, ');
+    } else if (title == 'Cage') {
+        document.write('Welcome Cage, ');
+    } else if (title == 'nic') {
+        document.write('Welcome Nic, ');
+    } else if (title == 'cage') {
+        document.write('Welcome Cage, ');
+    } else {
+        document.write('Welcome Nicolas Kim Coppola, ');
+    }
 
-if (title == 'Nic'){
-    document.write('Welcome Nic, ');
-} else if (title == 'Cage'){
-    document.write('Welcome Cage, ');
-} else if (title == 'nic'){
-    document.write('Welcome Nic, ');
-} else if (title == 'cage'){
-    document.write('Welcome Cage, ');
-} else {
-    document.write('Welcome Nicolas Kim Coppola, ');
 }
+
+enterName();
--- app3.js
+++ app3.js
@@ -1,8 +1,12 @@
-let userName = prompt('What is your name?')
-console.log(userName);
+function enterHiddenName() {
+    let userName = prompt('What is your name?')
+    console.log(userName);
 
-if (userName == 'uncle'){
-    document.write("Francis Ford Coppola");
-} else {
-    document.write(userName);
+    if (userName == 'uncle') {
+        document.write("Francis Ford Coppola");
+    } else {
+        document.write(userName);
+    }
 }
+
+enterHiddenName();
